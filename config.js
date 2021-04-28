3c540b5c3eb33728499c073e7298874bf1ab22f6
Karnadeep Rana <karnadeepmrana@gmail.com>
2021-04-28 16:11:36 +0530

    init

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://karna:karna123@cluster0.xgqnl.mongodb.net/merng?retryWrites=true&w=majority',
+    MONGODB1: 'mongodb+srv://anant00729:123123123@cluster0.i2anu.mongodb.net/merng?retryWrites=true&w=majority',
+    SECRET_KEY: 'some very secret key'
+}
